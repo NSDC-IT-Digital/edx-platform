1da7cadb336f36c8c5a1af1eff764d818fce7de9
Chandana030368 <chandana.kadiyala@drcsystems.com>
2025-05-16 19:44:49 +0530

    Update 03_video_player.js

--- xmodule/js/src/video/03_video_player.js
+++ xmodule/js/src/video/03_video_player.js
@@ -561,7 +561,7 @@
                   };
 
                   $.ajax({
-                      url: "/api/notes/v1/add_video_notes",
+                      url: "/api/notes/v1/video-notes",
                       method: "POST",
                       data: JSON.stringify(payload),
                       contentType: "application/json",
